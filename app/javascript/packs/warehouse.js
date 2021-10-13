f0d1445bdab541a94450664cb49f762cc47d00a5
Diego Borges <drborges.cic@gmail.com>
2021-10-13 18:22:59 -0300

    Remove unused imports

--- app/javascript/packs/warehouse.js
+++ app/javascript/packs/warehouse.js
@@ -1,7 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import createApp from '@shopify/app-bridge';
-import {Group, Scanner, Features} from '@shopify/app-bridge/actions';
 
 import App from "../scanner_app/App"
 
