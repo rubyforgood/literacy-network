3a1a15594e19aa0096817fc2380500462bdf8a69
daniel.mack <daniel.mack@powerhrg.com>
2021-10-13 16:26:51 -0400

    Add back ActiveRecord

--- book_warehouse/app/models/shop.rb
+++ book_warehouse/app/models/shop.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
-class Shop
+class Shop < ActiveRecord::Base
+  include ShopifyApp::ShopSessionStorageWithScopes
+
   def api_version
     ShopifyApp.configuration.api_version
   end
