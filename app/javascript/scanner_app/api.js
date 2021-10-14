0de694c0287be363334dda32bf36636930075144
Vinicius Gama <vinicius.sgama@gmail.com>
2021-10-14 16:59:23 -0300

    Replaces call to open library by our own book endpoint

--- app/javascript/scanner_app/api.js
+++ app/javascript/scanner_app/api.js
@@ -1,13 +1,12 @@
-const endpoint = isbn => `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`
+const endpoint = isbn => `/books/${isbn}`
 
 export const fetchBookInfo = async (isbn) => {
   const resp = await fetch(endpoint(isbn))
   const data = await resp.json()
-  const info = data[`ISBN:${isbn}`]
 
-  const title = info.title
-  const publishDate = info.publish_date
-  const authors = info.authors.map(author => author.name).join(", ")
+  const title = data.title
+  const publishDate = data.publish_date
+  const authors = data.authors.map(author => author.name).join(", ")
 
   return {
     isbn,
