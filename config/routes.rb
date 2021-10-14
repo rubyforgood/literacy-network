324c73ea0b7138f004767270079ca97be34b5782
Vinicius Gama <vinicius.sgama@gmail.com>
2021-10-14 13:41:05 -0300

    Adds dummy api endpoints to manipulate books

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
   get '/products', :to => 'products#index'
+
+  resources :books, only: %I[index show create update], param: :isbn
+
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
