322c9078937170a5a0e53e62ecc6b2a2128f845e
daniel.mack <daniel.mack@powerhrg.com>
2021-10-14 15:09:10 -0400

    Display book information

--- app/javascript/scanner_app/screens/ItemsScan.jsx
+++ app/javascript/scanner_app/screens/ItemsScan.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Stack, ButtonGroup, TextField, DisplayText, Form } from '@shopify/polaris';
 
 import "./styles.scss"
@@ -7,10 +7,12 @@ import { fetchBookInfo } from "../api";
 
 export default function ItemsScan() {
   const isbn = useInput()
+  
+  const [bookInfo, setBookInfo] = useState()
 
   const handleSubmit= async () => {
-    const data = await fetchBookInfo(isbn.value)
-    console.log(data)
+    const info = await fetchBookInfo(isbn.value)
+    setBookInfo(info)
   }
 
   return (
@@ -28,6 +30,7 @@ export default function ItemsScan() {
               </Stack.Item>
               <Button submit>Enter</Button>
             </Stack>
+            <code>{JSON.stringify(bookInfo, null, 2)}</code>
         </Stack>
       </div>
     </Form>
